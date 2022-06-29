2c0986726315243ebbe34a441a5195980611f2bf
a1426 <66330789+a1426@users.noreply.github.com>
2022-06-29 16:51:55 -0500

    Added a better way to remove the qs.

--- cvTest.py
+++ cvTest.py
@@ -4,7 +4,7 @@ import math
 from matplotlib import pyplot as plt
 from matplotlib import image
 import re
-
+from itertools import chain
 def find_gates(location,px):
     qubit_reg= {location: []}
     reg=re.compile("[0]{5,}")
@@ -13,10 +13,17 @@ def find_gates(location,px):
     for match in re.finditer(reg,color_string):
         qubit_reg[location].append(match.span())
     return qubit_reg
+def cut_off(img):
+    for index,column in enumerate(img.T):
+        prev=-1
+        if np.all(column==255) and True:
+            #Replace True with something more
+            print(index)
 
 def find_lines(inp, debug = None):
     pix_matrix = cv2.imread(inp,cv2.IMREAD_GRAYSCALE)
     #The "q" representing each qubit causes an extra edge to appear. This is a lazy fix. Make this more dynamic
+    cut_off(pix_matrix)
     pix_matrix=pix_matrix[:,100:]
     # These numbers are random, if the sensivity seems off, look here. Remove Magic Numbers.
     edges = cv2.Canny(pix_matrix, 80, 120)
@@ -42,15 +49,28 @@ def find_lines(inp, debug = None):
     if debug:
         cv2.imwrite(debug, pix_matrix)
     pure_lines = []
+    #Array of tuples that stores
+    corresponding_lines=[]
     other_lines=[]
     for i in range(len(hor_lines) - 1):
         # The 2 is the width of every line representing a qubit.
         if hor_lines[i + 1] - (v := hor_lines[i]) == 2:
-            pure_lines.append((v+1,pix_matrix[v + 1,:]))
+            pure_lines.append(v+1)
+            corresponding_lines.append((v+1,pix_matrix[v + 1,:]))
         elif hor_lines[i]-hor_lines[i-1]==2:
             pass
         else:
             other_lines.append(hor_lines[i])
+    #print(corresponding_lines)
     qubit_registry = {}
-    for y_pos,col in pure_lines:
+    for y_pos,col in corresponding_lines:
         qubit_registry.update(find_gates(y_pos,col))
+    print(qubit_registry)
+    print(other_lines)
+    print(pure_lines)
+    for key in qubit_registry.values():
+        print([0]+list(chain(*key)))
+find_lines("diagram2.png","lines3.png")
+
+
+
