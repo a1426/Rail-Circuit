fd99356c966379d54af6e3d89bb14fce749d48c1
a1426 <66330789+a1426@users.noreply.github.com>
2022-08-03 00:56:49 -0500

    Fixed so many issues

--- gate_finder.py
+++ gate_finder.py
@@ -5,6 +5,8 @@ from matplotlib import pyplot as plt
 from matplotlib import image
 import re
 from itertools import chain
+import os
+extra_margin=10
 def find_gates(location,px):
     locations=[]
     reg=re.compile("[0-]{5,}")
@@ -12,7 +14,13 @@ def find_gates(location,px):
     color_string=("".join(str(st)[0] for st in px))
     for match in re.finditer(reg,color_string):
         locations.append(match.span())
-    return {location:locations}
+    #The [0][0] is a precaition against false lines. All lines should start at the beggining, that is x=0
+    #False lines do not start at x=0, so this prevents that.
+    if locations and not locations[0][0]:
+        return {location:locations}
+    #A precaution against completely empty lines.
+    else:
+        return False
 def high_low(value,selection_list, default_height):
     try:
         higher = min([element for element in selection_list if element-value > 0])
@@ -22,6 +30,10 @@ def high_low(value,selection_list, default_height):
         lower = max(element for element in selection_list if element-value < 0)
     except ValueError:
         lower=0
+    if lower-extra_margin>=0:
+        lower-=extra_margin
+    if higher+extra_margin<=default_height:
+        higher+=extra_margin
     return lower,higher
 def cut_off(img):
     current_run=[]
@@ -35,9 +47,12 @@ def cut_off(img):
                 n_white.append(current_run)
                 current_run=[]
         prev=current
-    #The first element is the q, the second is the circuit
-    main_circuit=n_white[1]
+    #The last element is the circuit.
+    main_circuit=n_white[-1]
     return main_circuit[0]+1,main_circuit[-1]+1
+def clear():
+    for f in os.listdir("src2/img_save"):
+        os.remove(f"src2/img_save/{f}")
 def isolate_gates(inp, debug = ""):
     pix_matrix = cv2.imread(inp,cv2.IMREAD_GRAYSCALE) 
     begin, end=cut_off(pix_matrix)
@@ -81,16 +96,19 @@ def isolate_gates(inp, debug = ""):
             other_lines.append(hor_lines[i])
     qubit_registry = {}
     for y_pos,col in corresponding_lines:
-        qubit_registry.update(find_gates(y_pos,col))
+        if qubit:=find_gates(y_pos,col):
+            qubit_registry.update(qubit)
+    qubit_num=0
     for key,value in qubit_registry.items():
+        qubit_num+=1
         l,h = high_low(key,other_lines,height)
         flat_list = list(chain(*value))
-        flat_list.remove(0)
         flat_list.pop()
+        flat_list.remove(0)
         grouped_list = [flat_list[index:index + 2] for index in range(0,len(flat_list),2)]
         ct=0
         for left,right in grouped_list:
             ct+=1
             selection= pix_matrix[l:h,left:right]
-            with open(name:=f"img_save/{ct}.png","w+"):
+            with open(name:=f"src2/img_save/{qubit_num}-{ct}.png","w+"):
                 cv2.imwrite(name,selection)
