9b869379c2d09a1861c5e7e59c2433a2da719743
a1426 <66330789+a1426@users.noreply.github.com>
2022-08-04 00:11:49 -0500

    Update source_gen.py

--- source_gen.py
+++ source_gen.py
@@ -4,6 +4,8 @@ from random import randint, choice
 import matplotlib.pyplot as plt
 import gate_finder
 from collections import defaultdict
+from os import makedirs
+from json import dumps
 def random_component(obj):
     return choice([obj.x, obj.y, obj.z,obj.h, obj.i, obj.s, obj.sdg, obj.t, obj.tdg])
 class Simple_Square_Gates:
@@ -17,13 +19,20 @@ class Simple_Square_Gates:
             self.history[pos]+=1
             method(pos)
             self.gates[f'{pos}-{self.history[pos]}']=method.__name__
-        print(self.gates)
-
     def export(self):
         self.circuit.draw(output="mpl")
         plt.savefig("src2/generated_circuits/test.png")
-c1=Simple_Square_Gates(2)
-c1.export()
-gate_finder.clear()
-
-gate_finder.isolate_gates("src2/generated_circuits/test.png")
+    def generate_folder(self,path):
+        try:
+            makedirs(path)
+        except FileExistsError:
+            gate_finder.clear(path)
+        gate_finder.isolate_gates("src2/generated_circuits/test.png",path)
+        with open(f"{path}/labels.json","w+") as file:
+            data=dumps(self.gates)
+            file.write(data)
+def generate():
+    for x in range(10):
+        c1=Simple_Square_Gates(2)
+        c1.export()
+        c1.generate_folder(f"src2/img_save/{x}")
