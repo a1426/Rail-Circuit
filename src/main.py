bbb32d8da53ff60880fe118a19899a16e91a11d0
a1426 <66330789+a1426@users.noreply.github.com>
2024-08-09 02:15:37 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,23 +3,32 @@ from ultralytics import YOLO
 from PIL import Image
 from qiskit import QuantumCircuit
 
-def main(img):
-    #Names, to map the ids of the boxes onto the names.
-    with open("/Users/robert/Projects/datasets/testDataset/data.yaml") as file:
+#Names, to map the ids of the boxes onto the names.
+with open("/Users/robert/Projects/datasets/testDataset/data.yaml") as file:
         try:
             names=yaml.safe_load(file)["names"]
         except yaml.YAMLError as e:
             print(e)
+
+class single_gate():
+    def __init__(self, box):
+        self.act=""
+        self.x1,self.y1,self.x2,self.y2=box.xyxy[0]
+        self.conf=box.conf[0]
+        self.cls=box.cls[0]
+        self.name=names[int(self.cls)]
+        #Single gates, with no perameters.
+        if(self.cls<=10):
+            self.act=f"{self.name()}"
+        
+def find_qubits(im):
     #Find qubits
     #TODO:Fix.
-    qubits_y=[]
-    im=Image.open(img).convert("RGB")
-    width, height = im.size
     previously_colored=False
-    for y in range(height):
+    for y in range(im.size[1]):
         colored_column=False
         counter=0
-        for x in range(width):
+        for x in range(im.size[0]):
             r,g,b=im.getpixel((x,y))
             if(r==g==b<10):
                 counter+=1
@@ -33,40 +42,45 @@ def main(img):
         if(previously_colored and not colored_column):
             qubits_y[-1]=(qubits_y[-1][0]+y)//2
         previously_colored=colored_column
+    return qubits_y
 
+def find_y(gate):
+    for y in range(len(qubits_y)):
+        if gate.y1<qubits_y[y]<gate.y2:
+            qubits[y].append(gate)
+            return y
+
+
+def main(img):
+
+    im=Image.open(img).convert("RGB")
+    global qubits_y
+    qubits_y=[]
+    find_qubits(im)
 
     qc=QuantumCircuit(len(qubits_y))
+    global qubits
     qubits={x:[] for x in range(len(qubits_y))}
-
     ss_model = YOLO("models/squares/best.pt")
-
-    boxes=[]
-
     shapes=ss_model(img)[0]
+    single_gates=[]
     shapes.save(filename="result.jpg") #save to disk
     #Test for overlapping boxes
     for box in shapes.boxes:
-        for box2 in boxes:
-            c1=box.xyxyn[0]
-            c2=box2.xyxyn[0]
-            if (c1[0] <= c2[2] and c1[2] >= c2[0] and c1[1] <= c2[3] and c1[3] >= c2[1]):
-                if(box2.conf[0]<box.conf[0]):
-                    boxes.remove(box2)
-                    boxes.append(box)
-                break
-        else:
-            boxes.append(box)
-        if(len(boxes)==0):
-            boxes.append(box)
-    
-    boxes.sort(key=lambda box:box.xyxyn[0][0])
-    print(qubits_y)
-    for box in boxes:
-        for y in range(len(qubits_y)):
-            if box.xyxy[0][1]<qubits_y[y]<box.xyxy[0][3]:
-                qubits[y].append(box)
+        g1=single_gate(box)
+        y=find_y(g1)
+        for box2 in qubits[y]:
+            g2=single_gate(box2)
+            if box is not box2 and (g1.x1 <= g2.x2 and g1.x2 >= g2.x1 and g1.y1 <= g2.y2 and g1.y2 >= g2.y3):
+                if(g1.conf<g2.conf):
+                    qubits[y].remove(g1)
+                    qubits[y].add(g2)
+
+    single_gates.sort(key=lambda box:box.xyxy[0][0])
     
     for k,v in qubits.items():
         for box in v:
             exec(f"qc.{names[int(box.cls[0])]}({k})")
     return qc
+    
+#main("/Users/robert/Projects/datasets/testDataset/images/train/1.png")
