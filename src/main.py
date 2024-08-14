bd4ab9d3b7cd105d0cdb8b57f23a7d45c7106f95
a1426 <66330789+a1426@users.noreply.github.com>
2024-08-13 23:10:32 -0500

    Update main.py
    Added OCR(with tesseract) in order to capture the parameters for rotation gates. At the moment, the OCR is ~95% accurate with simple numbers, but it fails for pi.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,11 @@ import yaml
 from ultralytics import YOLO
 from PIL import Image
 from qiskit import QuantumCircuit
+import pytesseract
+import numpy as np
+import cv2
+import re
+
 
 #Names, to map the ids of the boxes onto the names.
 with open("datasets/square_dataset/data.yaml") as file:
@@ -13,13 +18,36 @@ with open("datasets/square_dataset/data.yaml") as file:
 class single_gate():
     def __init__(self, box):
         self.act=""
-        self.x1,self.y1,self.x2,self.y2=box.xyxy[0]
+        self.x1,self.y1,self.x2,self.y2=box.xyxy[0].numpy()
         self.conf=box.conf[0]
         self.cls=box.cls[0]
         self.name=names[int(self.cls)]
+
         #Single gates, with no perameters.
         if(self.cls<=10):
-            self.act=f"{self.name()}"
+            pass
+            #self.act=f"{self.name}.()"
+        else:
+            #TODO: Refine the approach with Tesseract.
+            greyscale=im.crop((self.x1,self.y1,self.x2,self.y2)).convert("L")
+            #Converts to greyscales
+            img=np.array(greyscale)
+            img=cv2.resize(img,None,fx=10,fy=10)
+            #img=cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+            kernel = np.ones((2, 2), np.uint8) 
+            img=cv2.erode(img, kernel, iterations=1)
+            exit()
+            num_q=method.__code__.co_argcount - (0 if method.__defaults__ is None else len(method.__defaults__))-2
+            
+            d=pytesseract.image_to_string(img,lang="eng+equ+grc",config=r'--oem 3 --psm 6')
+            cv2.imshow('image',img)
+            print(d)
+            cv2.waitKey(0)
+
+                    
+            #selection.show()
+            
+
         
 def find_qubits(im):
     #Find qubits
@@ -52,7 +80,7 @@ def find_y(gate):
 
 
 def main(img):
-
+    global im
     im=Image.open(img).convert("RGB")
     global qubits_y
     qubits_y=[]
@@ -69,16 +97,15 @@ def main(img):
     for box in shapes.boxes:
         g1=single_gate(box)
         y=find_y(g1)
-        for box2 in qubits[y]:
-            g2=single_gate(box2)
-            if box is not box2 and (g1.x1 <= g2.x2 and g1.x2 >= g2.x1 and g1.y1 <= g2.y2 and g1.y2 >= g2.y3):
+        for g2 in qubits[y]:
+            if g1.x1!= g2.x1 and g1.y1!=g2.y1 and g1.x1 <= g2.x2 and g1.x2 >= g2.x1 and g1.y1 <= g2.y2 and g1.y2 >= g2.y3:
                 if(g1.conf<g2.conf):
                     qubits[y].remove(g1)
                     qubits[y].add(g2)
-
     single_gates.sort(key=lambda box:box.xyxy[0][0])
     
     for k,v in qubits.items():
         for box in v:
             exec(f"qc.{names[int(box.cls[0])]}({k})")
     return qc
+main("datasets/square_dataset/images/train/26.png")
